ac25087aaa618f56cfcef27cffe4647d2cdf35e3
jomabe <dmari.scfe11@gmail.com>
2017-11-15 02:34:25 +0100

    Añadida función de buscar Invocador

--- app/src/main/java/com/dmb/pruebapi/MainActivity.java
+++ app/src/main/java/com/dmb/pruebapi/MainActivity.java
@@ -3,8 +3,8 @@ package com.dmb.pruebapi;
 import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,17 +14,15 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    private ListView fruitsList;
-    private String url = "https://euw1.api.riotgames.com/lol/summoner/v3/summoners/by-name/SongBird97?api_key=RGAPI-e6227e38-5d2d-4897-a068-b3d0ebf86018";
+
     private ProgressDialog dialog;
-    private TextView tv1;
+    private TextView tv1,tv2,tv3,tv4;
+    private EditText et1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,10 +31,21 @@ public class MainActivity extends AppCompatActivity {
 
 
         tv1 = (TextView)findViewById(R.id.tv1);
+        tv2 = (TextView)findViewById(R.id.tv2);
+        tv3 = (TextView)findViewById(R.id.tv3);
+        tv4 = (TextView)findViewById(R.id.tv4);
+        et1 = (EditText)findViewById(R.id.et1);
+
+    }
+
+    public void requestJsonData(View v){
 
         dialog = new ProgressDialog(this);
         dialog.setMessage("Cargando...");
         dialog.show();
+
+        String url = "https://euw1.api.riotgames.com/lol/summoner/v3/summoners/by-name/"+et1.getText().toString()+"?api_key=RGAPI-e6227e38-5d2d-4897-a068-b3d0ebf86018";
+
 
         StringRequest request = new StringRequest(url, new Response.Listener<String>() {
             @Override
@@ -46,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError volleyError) {
-                Toast.makeText(getApplicationContext(), "Ha ocurrido un error", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "No se ha encontrado el Invocador", Toast.LENGTH_SHORT).show();
                 dialog.dismiss();
             }
         });
@@ -55,11 +64,14 @@ public class MainActivity extends AppCompatActivity {
         rQueue.add(request);
     }
 
-    void parseJsonData(String jsonString) {
+    public void parseJsonData(String jsonString) {
+
         try {
             JSONObject object = new JSONObject(jsonString);
-            String name = object.optString("summonerLevel");
-            tv1.setText(name);
+            String name = object.optString("name");
+            String level = object.optString("summonerLevel");
+            tv3.setText(name);
+            tv4.setText(level);
         } catch (JSONException e) {
             e.printStackTrace();
         }
